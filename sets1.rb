ce5835e3c50c7575b9b2a5be8aa21faf91748e8c
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-07-11 01:52:01 +0900

    Fixed XOR

--- sets1.rb
+++ sets1.rb
@@ -12,6 +12,19 @@ class TestSets1 < Test::Unit::TestCase
   end
 
   # http://cryptopals.com/sets/1/challenges/2
+  def fixed_xor(buf1, buf2)
+    buf1.scan(/../).map(&:hex) # convert hex to integer
+      .map.with_index { |c, i| c ^ buf2.scan(/../)[i].hex } # xor
+      .map {|c| c.to_s(16) } # convert integer to hex
+      .join
+  end
+
   def test_fixed_xor
+    str1 = '1c0111001f010100061a024b53535009181c'
+    str2 = '686974207468652062756c6c277320657965'
+
+    actual = fixed_xor(str1, str2)
+    expect = '746865206b696420646f6e277420706c6179'
+    assert_equal expect, actual
   end
 end
